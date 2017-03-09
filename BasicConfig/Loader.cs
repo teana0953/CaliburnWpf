6d07320d1d0d4429f178905f34fad55229e03154
tina.liang <tina.liang@microlife.com.tw>
2017-03-09 13:04:11 +0800

    Loader

--- BasicConfig/Loader.cs
+++ BasicConfig/Loader.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BasicConfig
 {
-    class Loader
+    public class Loader : IResult
     {
+        readonly string message;
+        readonly bool hide;
+
+        public Loader(string message)
+        {
+            this.message = message;
+        }
+
+        public Loader(bool hide)
+        {
+            this.hide = hide;
+        }
+
+        public event EventHandler<ResultCompletionEventArgs> Completed;
+
+        public void Execute(CoroutineExecutionContext context)
+        {
+            var view = context.View as FrameworkElement;
+            while (view != null)
+            {
+                var busyIndicator = view as BusyIndicator;
+                if (busyIndicator != null)
+                {
+                    if (!string.IsNullOrEmpty(message))
+                        busyIndicator.BusyContent = message;
+                    busyIndicator.IsBusy = !hide;
+                    break;
+                }
+
+                view = view.Parent as FrameworkElement;
+            }
+
+            Completed(this, new ResultCompletionEventArgs());
+        }
     }
 }
